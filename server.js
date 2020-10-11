784f31e8ae20cdf92a4c6035384428147b3542bb
CallMeMrSam <samuelludmann57@gmail.com>
2020-10-11 17:47:11 +0200

    server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('/views'))
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    
+    res.render('index.ejs');
 });
 
 app.listen('4000', () => {
