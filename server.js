c59568c64b842090c9f1c5a78856b46b2b1ab5ca
Samuel-ldm <samuelludmann57@gmail.com>
2020-10-12 11:15:18 +0200

    frontend

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('/views'))
+const ARTICLES = [
+    {
+        title: 'Vive le node.js',
+        author: 'johnsmith',
+        contentShort: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+        date: Date.now()
+    },
+    {
+        title: 'Titre2',
+        author: 'johnsmith2',
+        contentShort: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+        date: Date.now()
+    }
+]
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', { articles: ARTICLES });
+});
+
+app.get('/post', (req, res) => {
+    res.render('post.ejs');
 });
 
 app.listen('4000', () => {
